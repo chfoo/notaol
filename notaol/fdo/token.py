b0999a8fb26360a09bd428eda951e68d74091eed
Christopher Foo <chris.foo@gmail.com>
2014-08-31 23:32:29 -0400

    Support showing token in data packet.

--- notaol/fdo/token.py
+++ notaol/fdo/token.py
@@ -1,6 +1,13 @@
 import enum
 
+from notaol.fdo.tokenmeta import TOKEN_METADATA
+
 
 class Token(enum.Enum):
     # TODO: put commonly used tokens here
     pass
+
+
+TOKEN_TO_DESC_MAP = dict([
+    (meta.token, meta.description) for meta in TOKEN_METADATA
+    ])
--- notaol/p3/data.py
+++ notaol/p3/data.py
@@ -1,12 +1,40 @@
+from notaol.fdo.token import TOKEN_TO_DESC_MAP
 from notaol.p3.payload import BasePayload
 
 
 class DataPayload(BasePayload):
+    '''FDO 91 Atom messages.
+
+    Attributes:
+        token (bytes): 2 bytes. The type of atom message.
+        data (bytes): The payload bytes of the atom message.
+        token_str (str): The token expressed as a string.
+    '''
     def __init__(self):
+        self.token = None
         self.data = None
 
+    @property
+    def token_str(self):
+        return self.token.decode('latin-1')
+
+    @token_str.setter
+    def token_str(self, token):
+        self.token = token.encode('latin-1')
+
     def parse(self, data):
-        self.data = data
+        self.token = data[:2]
+        self.data = data[2:]
 
     def to_bytes(self):
-        return self.data
+        return self.token + self.data
+
+    def __str__(self):
+        token_desc = TOKEN_TO_DESC_MAP.get(self.token_str)
+
+        if token_desc:
+            token_text = '{} ({})'.format(self.token_str, token_desc)
+        else:
+            token_text = self.token_str
+
+        return '<Data Payload Token={}>'.format(token_text)
